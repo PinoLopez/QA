91b92fc22c5c991fef007b9f9b84c0aa09478684
Pino <creadoresdigitales@gmail.com>
2025-03-22 17:22:04 +0100

    Añadidos comentarios y mejoras

--- playwright/demoblaze.spec.ts
+++ playwright/demoblaze.spec.ts
@@ -1,7 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'; // Importa las funciones 'test' y 'expect' de Playwright.
 
-test('Demo Blaze test', async ({ page }) => {
-  await page.goto('https://www.demoblaze.com/');
-  const title = await page.title();
-  expect(title).toContain('STORE');
+test('Demo Blaze test', async ({ page }) => 
+  { // Define un nuevo test llamado 'Demo Blaze test' y recibe el objeto 'page'.
+  await page.goto('https://www.demoblaze.com/'); // Navega a la URL de Demo Blaze.
+  const title = await page.title(); // Obtiene el título de la página.
+  expect(title).toContain('STORE'); // Verifica que el título contenga la palabra 'STORE'.
 });
